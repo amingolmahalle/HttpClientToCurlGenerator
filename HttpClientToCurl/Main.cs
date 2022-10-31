e564550e2b52c3bec1164454f863c7caca28df10
Amin GolMahaleh <A.GolMahaleh@persianswitch.net>
2022-10-31 09:53:50 +0330

    tiny change

--- HttpClientToCurl/Main.cs
+++ HttpClientToCurl/Main.cs
@@ -25,6 +25,7 @@ public static class Main
 
         return script;
     }
+    
     public static void GenerateCurlInConsole(this HttpClient httpClient, HttpRequestMessage httpRequestMessage, string requestUri = null, Action<ConsoleConfig> config = null)
     {
         var consoleConfig = new ConsoleConfig();
