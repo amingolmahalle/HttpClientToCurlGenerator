f35be871e1d095deea432511cba6bd1c11728739
Maryam Taheri <31124414+MaryamTaheri@users.noreply.github.com>
2024-04-29 17:39:24 +0330

    Update FailedCurlGeneratorTests.cs

--- tests/HttpClientToCurlGeneratorTest/UnitTest/MediaTypes/Json/FailedCurlGeneratorTests.cs
+++ tests/HttpClientToCurlGeneratorTest/UnitTest/MediaTypes/Json/FailedCurlGeneratorTests.cs
@@ -1,35 +1,28 @@
-// using FluentAssertions;
-// using System.Net.Mime;
-// using System.Text;
-// using HttpClientToCurl.Builder;
-// using Xunit;
-//
-// namespace HttpClientToCurlGeneratorTest.UnitTest.MediaTypes.Json;
-//
-// public class FailedCurlGeneratorTests
-// {
-//     [Fact]
-//     public void GenerateCurl_When_HttpMethod_Is_Invalid()
-//     {
-//         // Arrange
-//         string requestBody = /*lang=json,strict*/ @"{""name"":""russel"",""requestId"":10001004,""amount"":50000}";
-//
-//         var requestUri = "api/test";
-//         var httpRequestMessage = new HttpRequestMessage(HttpMethod.Trace, requestUri) { Content = new StringContent(requestBody, Encoding.UTF8, MediaTypeNames.Application.Json) };
-//         httpRequestMessage.Headers.Add("Authorization", "Bearer 4797c126-3f8a-454a-aff1-96c0220dae61");
-//
-//         using var httpClient = new HttpClient();
-//         httpClient.BaseAddress = new Uri("http://localhost:1213/v1/");
-//
-//         // Act
-//         string script = Generator.GenerateCurl(
-//             httpClient,
-//             httpRequestMessage,
-//             null);
-//
-//         // Assert
-//         script.Should().NotBeNullOrEmpty();
-//         script.Should().StartWith("GenerateCurlError => not supported");
-//         script.Trim().Should().BeEquivalentTo($"GenerateCurlError => not supported {httpRequestMessage.Method.Method} by HttpClientToCurl!");
-//     }
-// }
+using HttpClientToCurl.Builder.Concrete;
+using HttpClientToCurl.Builder.Interface;
+using HttpClientToCurl.Config;
+
+namespace HttpClientToCurl.Builder;
+
+public static class Generator
+{
+    public static string GenerateCurl(HttpClient httpClient, HttpRequestMessage httpRequestMessage, BaseConfig config)
+    {
+        var builder = GetBuilder(httpRequestMessage.Method);
+        return builder.CreateCurl(httpClient, httpRequestMessage, config);
+    }
+
+    private static IBuilder GetBuilder(HttpMethod method)
+    {
+        string methodName = method.Method;
+        return methodName switch
+        {
+            "GET" => new HttpGetBuilder(),
+            "POST" => new HttpPostBuilder(),
+            "PUT" => new HttpPutBuilder(),
+            "PATCH" => new HttpPatchBuilder(),
+            "DELETE" => new HttpDeleteBuilder(),
+            _ => throw new Exception($"HTTP method {method} is not supported."),
+        };
+    }
+}
