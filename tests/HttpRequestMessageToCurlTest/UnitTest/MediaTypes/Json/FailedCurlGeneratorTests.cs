7d18a69cd00e4861e609a3bd7b057db8c586c2ed
amingolmahalle <amin.golmahalle@gmail.com>
2025-10-17 15:32:02 +0330

    Add HttpRequestMessageToCurlTest into tests folder

---
+++ tests/HttpRequestMessageToCurlTest/UnitTest/MediaTypes/Json/FailedCurlGeneratorTests.cs
@@ -0,0 +1,31 @@
+using System.Net.Mime;
+using System.Text;
+using HttpClientToCurl.Builder;
+using Xunit;
+
+namespace HttpRequestMessageToCurlTest.UnitTest.MediaTypes.Json;
+
+public class FailedCurlGeneratorTests
+{
+    [Fact]
+    public void GenerateCurl_When_HttpMethod_Is_Invalid()
+    {
+        // Arrange
+        string requestBody = /*lang=json,strict*/ @"{""name"":""russel"",""requestId"":10001004,""amount"":50000}";
+
+        var requestUri = "api/test";
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Trace, requestUri)
+        {
+            Content = new StringContent(requestBody, Encoding.UTF8, MediaTypeNames.Application.Json)
+        };
+        httpRequestMessage.Headers.Add("Authorization", "Bearer 4797c126-3f8a-454a-aff1-96c0220dae61");
+
+        var baseAddress = new Uri("http://localhost:1213/v1/");
+
+        // Act - Assert
+        Assert.Throws<NotSupportedException>(() => Generator.GenerateCurl(
+            httpRequestMessage,
+            baseAddress,
+            null));
+    }
+}
